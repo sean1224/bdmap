bd4dedfa37e885eddca0b5c4111124a6060d3e81
sean1224 <sean.lijian@gmail.com>
2019-06-04 09:29:17 +0800

    第一次提交

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+import App from './App'
+// import baiduMap from 'vue-baidu-map'
+import '@/assets/styles/reset.css'
+
+Vue.config.productionTip = false
+// Vue.use(baiduMap, {
+//   ak: 'qKETBIx5q33Yv4OBNSrxgAG4ByPfgi7g'
+// })
+let vm = new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>'
+})
+Vue.use({vm})
---
+++ src/pages/main/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <baidu-map class="map">
+  </baidu-map>
+</template>
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style lang='scss' scoped>
+.map{
+  position: absolute;
+  left:0;
+  top:0;
+  width:100%;
+  height:100%;
+  background-color: red;
+}
+</style>
